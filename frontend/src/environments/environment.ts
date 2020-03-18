e21de50b8ee62e4b0e364adc58cd46b243541320
DinaTaklit <ez_taklit@esi.dz>
2020-03-18 02:29:29 +0100

    feat: Configure Enviornment Variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'coffee-shop-application', // the auth0 domain prefix
+    audience: 'coffee-shop-app', // the audience set for the auth0 app
+    clientId: '7uO05f5wr8954Wer0MjLz4YabjkC36U5', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
