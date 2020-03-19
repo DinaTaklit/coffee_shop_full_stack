e590b77b28a55862e61cb10bf48d78c1c154f493
DinaTaklit <ez_taklit@esi.dz>
2020-03-19 20:33:31 +0100

    fix: Remove init from models rizing 422 error

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -41,11 +41,7 @@ class Drink(db.Model):
     # the ingredients blob - this stores a lazy json blob
     # the required datatype is [{'color': string, 'name':string, 'parts':number}]
     recipe =  Column(String(180), nullable=False)
-
-    def __init__(self, title, recipe):
-        self.title = title
-        self.reciepe = recipe
-
+    
     '''
     short()
         short form representation of the Drink model
